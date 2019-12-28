da59137ec096ca96f748560b3f201faa66791179
Sivanantham Padikkasu <pksivanatham@gmail.com>
2019-12-28 13:37:46 +0530

    1.Path name is case sensitive in linux base distro . 2.Updated the path name

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
+//const http = require('http')
 
 const HOSTNAME = '127.0.0.1';
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello dev.to!\n');
-// });
+//  const server = http.createServer((req, res) => {
+//    res.statusCode = 200;
+//    res.setHeader('Content-Type', 'text/plain');
+//    res.end('Hello dev.to!\n');
+//  });
 
 app.listen(PORT, HOSTNAME, () => {
   console.log(`Server running at ${HOSTNAME} on port ${PORT}.`);
